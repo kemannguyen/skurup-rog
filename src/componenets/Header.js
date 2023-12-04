f863d50bbe385a00055034e232fcc9ddc7d4f299
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2023-12-05 00:43:36 +0100

    Update Header.js

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -8,22 +8,28 @@ import NavigationLayer from "./NavigationLayer";
 const Header = () => {
   const [menu, setMenu] = useState(false);
   const openMenu = () => setMenu(!menu);
+  const [tabindex, setindex] = useState(0);
 
   const navigate = useNavigate();
   const TillHem = () => {
     navigate("/");
+    setindex(0);
   };
   const TillMeny = () => {
     navigate("/meny");
+    setindex(1);
   };
   const TillOmOss = () => {
     navigate("/om-oss");
+    setindex(2);
   };
   const TillGalleri = () => {
     navigate("/galleri");
+    setindex(3);
   };
   const TillKontaktaOss = () => {
     navigate("/kontakta-oss");
+    setindex(4);
   };
 
   const screenWidth = () => {
@@ -31,6 +37,89 @@ const Header = () => {
       setMenu(false);
     }
   };
+
+  let homebtn;
+  if (tabindex == 0) {
+    homebtn = (
+      <span className="navbtn-active" onClick={TillHem}>
+        {" "}
+        hem
+      </span>
+    );
+  } else {
+    homebtn = (
+      <span className="navbtn" onClick={TillHem}>
+        {" "}
+        hem
+      </span>
+    );
+  }
+
+  let menubtn;
+  if (tabindex == 1) {
+    menubtn = (
+      <span className="navbtn-active" onClick={TillMeny}>
+        {" "}
+        meny
+      </span>
+    );
+  } else {
+    menubtn = (
+      <span className="navbtn" onClick={TillMeny}>
+        {" "}
+        meny
+      </span>
+    );
+  }
+
+  let omossbtn;
+  if (tabindex == 2) {
+    omossbtn = (
+      <span className="navbtn-active" onClick={TillOmOss}>
+        {" "}
+        om oss
+      </span>
+    );
+  } else {
+    omossbtn = (
+      <span className="navbtn" onClick={TillOmOss}>
+        {" "}
+        om oss
+      </span>
+    );
+  }
+  let galleribtn;
+  if (tabindex == 3) {
+    galleribtn = (
+      <span className="navbtn-active" onClick={TillGalleri}>
+        {" "}
+        galleri
+      </span>
+    );
+  } else {
+    galleribtn = (
+      <span className="navbtn" onClick={TillGalleri}>
+        {" "}
+        galleri
+      </span>
+    );
+  }
+  let kontaktaossbtn;
+  if (tabindex == 4) {
+    kontaktaossbtn = (
+      <span className="navbtn-active" onClick={TillKontaktaOss}>
+        {" "}
+        kontakta oss
+      </span>
+    );
+  } else {
+    kontaktaossbtn = (
+      <span className="navbtn" onClick={TillKontaktaOss}>
+        {" "}
+        kontakta oss
+      </span>
+    );
+  }
 
   useEffect(() => {
     window.addEventListener("resize", screenWidth);
@@ -43,26 +132,11 @@ const Header = () => {
         Skurup Restaurang & Grill
       </span>
       <div className="navbtns">
-        <span className="navbtn" onClick={TillHem}>
-          {" "}
-          hem
-        </span>
-        <span className="navbtn" onClick={TillMeny}>
-          {" "}
-          meny
-        </span>
-        <span className="navbtn" onClick={TillOmOss}>
-          {" "}
-          om oss
-        </span>
-        <span className="navbtn" onClick={TillGalleri}>
-          {" "}
-          galleri
-        </span>
-        <span className="navbtn" onClick={TillKontaktaOss}>
-          {" "}
-          kontakta oss
-        </span>
+        {homebtn}
+        {menubtn}
+        {omossbtn}
+        {galleribtn}
+        {kontaktaossbtn}
       </div>
       <img
         src={menu ? xicon : icon}
