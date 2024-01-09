8a02d4b63d1ddd7cef970e55776d14f1fda76437
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2024-01-09 15:14:39 +0100

    feat: added fb to contact page

--- src/componenets/ContactUs.js
+++ src/componenets/ContactUs.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import locicon from "../images/location-icon.png";
 import phoneicon from "../images/phone-icon.png";
+import fbicon from "../images/fb-icon.png";
 import "../style/ContactUs.css";
 
 const ContactUs = () => {
@@ -25,12 +26,13 @@ const ContactUs = () => {
           <div className="flex justify-center">
             <img
               src={locicon}
-              className="contact-img adress"
+              className="contact-img clickable"
               onClick={() =>
                 openLinkInNewTab(
                   "https://www.google.com/maps/place/Asklidenv%C3%A4gen+1,+274+37+Skurup/@55.4934515,13.50497,17z/data=!3m1!4b1!4m6!3m5!1s0x46538199ab89fe27:0xd3e8938d77c65ad0!8m2!3d55.4934515!4d13.50497!16s%2Fg%2F11c1ktsk99?entry=ttu"
                 )
               }
+              alt=""
             />
           </div>
           <p className="text-2xl font-strong text-center mt-3">Adress:</p>
@@ -41,7 +43,26 @@ const ContactUs = () => {
         </div>
         <div className="flex flex-col">
           <div className="flex justify-center">
-            <img src={phoneicon} className="contact-img" />
+            <img
+              src={fbicon}
+              className="contact-img clickable"
+              onClick={() =>
+                openLinkInNewTab(
+                  "https://www.facebook.com/profile.php?id=100063565211709"
+                )
+              }
+              alt=""
+            />
+          </div>
+          <p className="text-2xl font-strong text-center mt-3">Facebook:</p>
+          <span className="text-2xl font-strong text-center">
+            {" "}
+            Skurup Restaurang& Grill
+          </span>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex justify-center">
+            <img src={phoneicon} className="contact-img" alt="" />
           </div>
           <p className="text-2xl font-strong text-center mt-3">Telefon:</p>
           <span className="text-2xl font-strong text-center">
--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -51,6 +51,7 @@ const Home = () => {
             <img
               className="responsive-img"
               src="https://img.freepik.com/free-vector/golden-sparkling-2024-new-year-eve-lettering-background-design-vector_1017-46452.jpg?w=826&t=st=1704680330~exp=1704680930~hmac=ba705918707bf3ba17bced6a90c1518605690340a305de2f06aa47207b9e121b"
+              alt=""
             />
           </div>
           <div className=" max-w-[300px] p-5 font-handwritten responsive-text">
