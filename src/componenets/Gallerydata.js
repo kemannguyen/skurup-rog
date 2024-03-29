e3f915d9d94e9f2b7fd732e434f0a4d3fb77c891
Keman Nguyen <62680940+kemannguyen@users.noreply.github.com>
2024-03-30 00:40:54 +0100

    fix: images link

--- src/componenets/Gallerydata.js
+++ src/componenets/Gallerydata.js
@@ -2,79 +2,37 @@ export const GalleryData = [
   {
     title: "Skurup Restaurang & Grill",
     imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t39.30808-6/277672424_388941409901363_9130234496202582665_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=SxokIOYgefUAX8IO9Lk&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfA1Ctt0ONd3ikt84J0G4W5kS9E1cJHUwMRQhJNbNBrPyQ&oe=65E2420A",
+      "https://i.imgur.com/hPmPZX4.jpeg",
     description: "Såhär ser vår lokal ut (2019)",
   },
   {
     title: "Pytt i panna med ägg och rödbetor",
     imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29511122_346488935856678_7711177480290327427_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=uCSwK4zZRZcAX_7EP5Y&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfCoo4BrDzuiOFpHvQbeeAk9oODOqTer6pqcpeWZLt9TVg&oe=660417B4",
+      "https://i.imgur.com/tAi4QKY.jpeg",
     description: "",
   },
   {
     title: "Honungsglaserade revbensspjäll med skysås och äppelmos",
     imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29511355_346488915856680_6538949167608295152_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=4YSgqmOxrWQAX_wSTFg&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfBEF4LaU9WxFLR6qKVFmPCk5ydwGZoLNUgowKlEB5x9BQ&oe=66042DD7",
+      "https://i.imgur.com/JD4dfmk.jpeg",
     description: "",
   },
   {
     title: "Grillbiff med stekt potatis och bearnaisesås",
     imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29468167_346081152564123_8033025033580741009_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=r-OO-B1rb1AAX_IoIiV&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfB5c_ZOfd5uoKbIU8-Ob-kO9zxJj8d1tgn4Gj65nfE69A&oe=6603FF95",
+      "https://i.imgur.com/p8hlIyY.jpeg",
     description: "",
   },
   {
     title: "Fläskschnitzel med stekt potatis och bearnaisesås",
     imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29570422_346927432479495_979945463841563737_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=nEH6RMPd0cQAX9finRF&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfAj5D7vImzfnGYcNKA8KkmNp9QmmG-gG9m8W_BKwDZirA&oe=6603FAC5",
-    description: "",
-  },
-  {
-    title: "Pannbiff med kokt potatis och brunsås",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29433105_346081122564126_2366932989748214945_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=5qnlAk4NykAAX9XQo5o&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfDUQ4zQz6n8pDMGrlcvkYnc-aXeTEH5n4af0N6HGI_3xA&oe=6604181F",
-    description: "",
-  },
-  {
-    title: "Korvstroganoff",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29495949_345649732607265_5299520364027908787_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=37mF03q30SoAX92_CGK&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfDurufg6p7fPGqBANDvT7j2mM5atluQWQ17fLSxdWP_nA&oe=6604315A",
+      "https://i.imgur.com/Bx2yeiJ.jpeg",
     description: "",
   },
   {
     title: "Panerad spätta med kokt potatis och remouladsås",
     imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t1.18169-9/29386201_345649702607268_6218526821052446112_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=YeESr63P3pcAX9rqGGa&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfCWxubouPQ_xMcPvZX5S1VRuxk2aGB3RhjkbvrUJqBM5g&oe=66041FB4",
-    description: "",
-  },
-  {
-    title: "Fläsknoisette med stekt potatis och bearnaisesås",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t31.18172-8/28827011_345208065984765_1031530687805210595_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=XSMbEa4v_sQAX_5LRAU&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfCdSOouahwl7s15ighfkiLlMgwn7CezJYQlcb4vPDd1Ow&oe=66041BCD",
-    description: "",
-  },
-  {
-    title: "Stekt fläsk med kokt potatis och brunsås",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t31.18172-8/29063891_343998559439049_7291269498609153228_o.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=HnwsbLuM-DcAX9OU56z&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfA2e9QPF40YP8c1QNVTTpA9eiJdv6TuYW5V2PAlOTqFtw&oe=6604213B",
-    description: "",
-  },
-  {
-    title: "Kalops med kokt potatis och rödbetor",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t31.18172-8/28827223_343589022813336_7079474536458642260_o.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=661ZpLjcmS0AX81RHCe&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfDhVtSIW2frYsTv_X2ZkuGxMS15DEFUJHZuqmAR5gdSjg&oe=66042089",
-    description: "",
-  },
-  {
-    title: "Fylld lunchkorv med hemlagad potatismos och lingon",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t31.18172-8/28701418_343589012813337_477751365528977204_o.jpg?_nc_cat=108&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=2QP268whf_kAX-gK48-&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfC7hi1bd3ZEMPnXYOrdqCAYH6thtBrFpeJ-n3lYevrpLQ&oe=660431D4",
-    description: "",
-  },
-  {
-    title: "Stekt falukorv med ägg och stekt potatis",
-    imgsrc:
-      "https://scontent.farn2-2.fna.fbcdn.net/v/t31.18172-8/29871715_353066541865584_1853706755644161512_o.jpg?_nc_cat=111&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=NPHu1yZnZ_AAX_6awXt&_nc_ht=scontent.farn2-2.fna&cb_e2o_trans=t&oh=00_AfAigQZ2t-QtqO3lYBD7KZI6k2oxPBBJpf7mapCJhFjKGw&oe=6604166D",
+      "https://i.imgur.com/zUW2GFQ.jpeg",
     description: "",
   },
 ];
